40743f7e1a4bbd11215dcf8746fdb9491bff3b00
Isaac Cortes Hernandez <isaaccortes014@gmail.com>
2023-10-24 11:30:07 -0700

    added calculation and results to the page

--- scripts/annuity.js
+++ scripts/annuity.js
@@ -1,3 +1,24 @@
 function calculatePresValAnnuity() {
-  
+  let mPayout = parseFloat(document.getElementById('mPayout').value);
+  let interestRate =
+    parseFloat(document.getElementById('interestRate').value) / 100;
+  let years = parseInt(document.getElementById('years').value);
+  let totalPayments = years * 12;
+  let interestMonthly = interestRate / 12;
+
+  let presentValue =
+    (mPayout * (1 - Math.pow(1 + interestMonthly, -totalPayments))) /
+    interestMonthly;
+
+  document.getElementById('presentValue').value = presentValue.toFixed(2);
+
+  document.getElementById(
+    'resultText'
+  ).textContent = `To fund an annuity that pays $${mPayout.toFixed(
+    2
+  )} monthly for ${years} years and earns an expected
+  ${(interestRate * 100).toFixed(
+    2
+  )}% interest, you would need to invest $${presentValue.toFixed(2)} today.
+`;
 }
